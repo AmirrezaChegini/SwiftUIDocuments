2350489d1f555def63c8eacb9bcb25eafe57cff4
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2026-07-31 17:30:05 +0330

    add text view

---
+++ SwiftUIDocuments/BaseViews/View01_Text.swift
@@ -0,0 +1,35 @@
+//
+//  View01_Text.swift
+//  SwiftUIDocuments
+//
+//  Created by Amirreza on 1405/5/9.
+//
+
+import SwiftUI
+
+struct View01_Text: View {
+    var body: some View {
+        Text("Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! ")
+//            .font(.largeTitle)  // use pre font size
+//            .fontDesign(.serif) // use pre font design
+//            .fontWeight(.heavy) // change font weight
+//            .font(.system(      // use system for custom font style
+//                size: 20,       // change font size
+//                weight: .heavy, // change font weight
+//                design: .serif, // change font design
+//            ))
+//            .bold()             //.bold == .fontDesign(.bold)
+//            .underline()        // underline text
+//            .italic()           // italic font
+//            .strikethrough()    // line through the text
+//            .baselineOffset(30) // Creating vertical spacing between the text and the starting point
+//            .multilineTextAlignment(.center) // alignment for multiline text
+//            .kerning(2)         // space between each words
+//            .foregroundStyle(.blue) // change text color
+//            .minimumScaleFactor(0.6) // To display the full text at frames
+    }
+}
+
+#Preview {
+    View01_Text()
+}
---
+++ SwiftUIDocuments/SwiftUIDocumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDocumentsApp.swift
+//  SwiftUIDocuments
+//
+//  Created by Amirreza on 1405/5/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDocumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            View01_Text()
+        }
+    }
+}
