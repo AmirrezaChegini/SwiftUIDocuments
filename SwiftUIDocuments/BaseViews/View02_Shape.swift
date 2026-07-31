ad94a2defba1802c4322597c803c7307cf8e44b7
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2026-07-31 17:49:34 +0330

    add shape view

---
+++ SwiftUIDocuments/BaseViews/View02_Shape.swift
@@ -0,0 +1,18 @@
+//
+//  View02_Shape.swift
+//  SwiftUIDocuments
+//
+//  Created by Amirreza on 1405/5/9.
+//
+
+import SwiftUI
+
+struct View02_Shape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View02_Shape()
+}
