1774f16e39d4cbb0137dc5f5cb3f277770a7acdc
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2026-07-31 17:54:10 +0330

    add text view

--- SwiftUIDocuments/BaseViews/View02_Shape.swift
+++ SwiftUIDocuments/BaseViews/View02_Shape.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct View02_Shape: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+//        Ellipse()
+//        Capsule()
+//        Rectangle()
+//        RoundedRectangle(cornerRadius: 0)
+//            .fill(.red)    // we can use only one of the fill or stroke
+//            .stroke()      // border shape
+//            .stroke(.red)  // change border color
+//            .stroke(.red, lineWidth: 20)  // change border color & width
+//            .stroke(style: StrokeStyle(
+//                lineWidth: 20, // change border width
+//                lineCap: .round, // change each peace of border shape
+//                dash: [30],   // space between peace of border
+//                dashPhase: 0, // angle of border 0 - 360
+//            ))
+//            .trim(from: 0.4, to: 0.8)  // cutting peace of shape 0 - 1
     }
 }
 
