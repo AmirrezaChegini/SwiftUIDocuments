39e3638bb360874770da8a4f0545de12573fe146
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2026-08-06 09:57:51 +0330

    add contaner relative frame

--- SwiftUIDocuments/BaseViews/View08_Frame.swift
+++ SwiftUIDocuments/BaseViews/View08_Frame.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct View08_Frame: View {
     var body: some View {
         // frame view can used for any views you can set size and alignment for any views
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
 //            .frame(
 //                minWidth: 100,
 //                idealWidth: 80,
@@ -20,8 +20,14 @@ struct View08_Frame: View {
 //                maxHeight: 100,
 //                alignment: .center
 //            )
-        
 //            .frame(width: 100, height: 100, alignment: .center)
+        // these are for adaptive size based on screen
+//            .containerRelativeFrame(.horizontal) { length, axis in
+//                length * 0.8
+//            }
+//            .containerRelativeFrame(.vertical) { length, axis in
+//                length * 0.5
+//            }
     }
 }
 
